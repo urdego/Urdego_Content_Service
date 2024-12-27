025943aa481eaba4f1809141bb0a42397e18e541
JoungWoo Lee <j_ra1n@naver.com>
2024-12-27 16:02:50 +0900

    Init: 프로젝트 초기세팅

---
+++ src/main/java/io/urdego/urdego_content_service/UrdegoContentServiceApplication.java
@@ -0,0 +1,13 @@
+package io.urdego.urdego_content_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrdegoContentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrdegoContentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/urdego/urdego_content_service/UrdegoContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.urdego.urdego_content_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrdegoContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
