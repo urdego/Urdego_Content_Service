8c96ddb7d43bfb3f0b16a509dc6a3d53b974a843
JoungWoo Lee <j_ra1n@naver.com>
2025-01-10 15:13:07 +0900

    BASE_URL 수정

--- src/main/java/io/urdego/urdego_content_service/domain/service/ContentCommander.java
+++ src/main/java/io/urdego/urdego_content_service/domain/service/ContentCommander.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 public class ContentCommander {
 
     public static final Path BASE_PATH = Path.of("/urdego/user/content");
-    public static final String BASE_URL = "https://urdego.site/urdego/user/content";
+    public static final String BASE_URL = "https://urdego.site/file/user/content";
 
     // 컨텐츠 저장
     public static FileInfo saveContent(Long userId, MultipartFile content) {
