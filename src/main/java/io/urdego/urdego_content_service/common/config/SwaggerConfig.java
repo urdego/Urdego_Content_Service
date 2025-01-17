748501c8f1cd1df24be828b9221fa2589239bbac
JoungWoo Lee <j_ra1n@naver.com>
2025-01-17 14:01:28 +0900

    Swagger Gateway 통합

---
+++ src/main/java/io/urdego/urdego_content_service/common/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package io.urdego.urdego_content_service.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("어데고 컨텐츠 API")
+                        .version("v3")
+                        .description("어데고 컨텐츠 서버")
+                        .contact(new Contact()
+                                .name("📍 어데고 GitHub Link")
+                                .url("https://github.com/urdego"))
+                        .license(new License()
+                                .name("⚖️ Apache License Version 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0")))
+
+                .servers(List.of(
+                        new Server().url("http://localhost:8081"),
+                        new Server().url("https://urdego.site")
+                ));
+    }
+}
