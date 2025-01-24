f2ead67fca53080c3282ccd0eaaca027fe1295b9
JoungWoo Lee <j_ra1n@naver.com>
2025-01-24 21:06:54 +0900

    feat(#23): 컨텐츠 유해성 감지

--- src/main/java/io/urdego/urdego_content_service/common/exception/ExceptionMessage.java
+++ src/main/java/io/urdego/urdego_content_service/common/exception/ExceptionMessage.java
@@ -14,9 +14,11 @@ public enum ExceptionMessage {
     // Content save
     INVALID_FILE_FORMAT("잘못된 형식의 컨텐츠입니다."),
     CONTENT_SAVE_FAILED("컨텐츠 저장에 실패했습니다."),
-    CONTENT_MULTI_SAVE_FAILED("컨텐츠 다중 저장에 실패했습니다."),
+    CONTENT_MULTI_SAVE_FAILED("컨텐츠 다중 저장에 실패했습니다. (컨텐츠 유해성 감지)"),
     CONTENT_DELETE_FAILED("컨텐츠 삭제에 실패했습니다."),
     DIRECTORY_CREATION_FAILED("상위 폴더 생성 중 예외가 발생했습니다."),
+    CONTENT_NOT_ALLOWED("컨텐츠에 유해성이 감지되어 저장에 실패했습니다."),
+    CONTENT_ANALYSIS_FAILED("컨텐츠 분석에 실패했습니다."),
 
     // Image
     IMAGE_METADATA_FAILED("이미지 메타데이터 추출에 실패했습니다.");
