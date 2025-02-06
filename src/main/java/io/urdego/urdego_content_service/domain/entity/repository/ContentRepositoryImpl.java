f3eeca9a8c80590dffcf105fa9c9f18777cb0566
JoungWoo Lee <j_ra1n@naver.com>
2025-02-06 16:03:25 +0900

    feat(#27): 컨텐츠 조회 최신순 추가

--- src/main/java/io/urdego/urdego_content_service/domain/entity/repository/ContentRepositoryImpl.java
+++ src/main/java/io/urdego/urdego_content_service/domain/entity/repository/ContentRepositoryImpl.java
@@ -20,7 +20,8 @@ public class ContentRepositoryImpl implements ContentRepositoryCustom {
 
     // userId를 통해 해당 유저의 컨텐츠를 조회한다.
     @Override
-    public List<ContentResponse> findUserContentsByUserId_CursorPaging(Long userId, Long cursorIdx, Long limit) {
+    public List<ContentResponse> findUserContentsByUserId_CursorPaging(Long userId, Long cursorIdx, Long limit, String sortBy) {
+
 
         JPAQuery<ContentResponse> query =
                 queryFactory.select(Projections.constructor(ContentResponse.class,
@@ -30,13 +31,26 @@ public class ContentRepositoryImpl implements ContentRepositoryCustom {
                                 content.address,
                                 content.latitude,
                                 content.longitude,
-                                content.hint))
+                                content.hint,
+                                content.createdDateTime))
                         .from(content)
-                        .where(content.userId.eq(userId))
-                        .orderBy(content.id.asc());
+                        .where(content.userId.eq(userId));
 
-        if (cursorIdx != null) {
-            query = query.where(content.id.gt(cursorIdx)); // 먼저 등록한순 (마지막 항목기준 Id 값이 큰값들 조회)
+        if(sortBy.equals("recent"))
+        {
+            query = query.orderBy(content.createdDateTime.desc(), content.id.desc());
+
+            if (cursorIdx != null) {
+                // 커서 기반 페이징: contentId가 cursorIdx보다 작은 데이터만 조회 (최신순)
+                query = query.where(content.id.lt(cursorIdx));
+            }
+        } else if(sortBy.equals("oldest"))
+        {
+            query = query.orderBy(content.createdDateTime.asc(), content.id.asc());
+
+            if (cursorIdx != null) {
+                query = query.where(content.id.gt(cursorIdx)); // 먼저 등록한순 (마지막 항목기준 Id 값이 큰값들 조회)
+            }
         }
 
         return query.limit(limit).fetch();
